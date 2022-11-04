607eac474f72b05fd16c1bd648f0803ce9361c0f
nikfp <nikprotsman@gmail.com>
2022-11-04 17:37:48 -0600

    cleanups all around

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -8,11 +8,11 @@ end
 
 --shortcuts for Telescope
 
-map("n", "ff", ":Telescope find_files<CR>")
-map("n", "fg", ":Telescope live_grep<CR>")
-map("n", "fb", ":Telescope buffers<CR>")
-map("n", "fh", ":Telescope help_tags<CR>")
-map("n", "fi", ":Telescope file_browser<CR>")
+map("n", "<leader>ff", ":Telescope find_files<CR>")
+map("n", "<leader>fg", ":Telescope live_grep<CR>")
+map("n", "<leader>fb", ":Telescope buffers<CR>")
+map("n", "<leader>fh", ":Telescope help_tags<CR>")
+map("n", "<leader>fi", ":Telescope file_browser<CR>")
 
 -- <<< GIT Stuff >>>
 
@@ -30,7 +30,9 @@ map("i", "<C-j>", "<Down>")
 map("i", "<C-K", "<Up>")
 -- move lines up and down
 map('n', '<A-k>', ":m .-2<cr>") -- up
+map('v', '<A-k>', ":m .-2<cr>") -- up
 map('n', "<A-j>", ":m .+1<cr>") -- down
+map('v', "<A-j>", ":m .+1<cr>") -- down
 -- Save and source current buffer
 map('n', '<leader><leader>x', ":w<cr>:source %<cr>")
 -- Format with prettier
@@ -39,14 +41,12 @@ map('n', '<leader>shaye', ':echo "shaye is awesome"<cr>')
 -- Mind.nvim maps
 map('n', '<leader>mi', ':MindOpenSmartProject<cr>')
 map('n', '<leader>mc', ':MindClose<cr>')
--- <<< Other locations for keymaps >>>
+-- <<< rther locations for keymaps >>>
 -- Tried to avoid, but sometimes it makes sense in the modules
 -- All paths relative to here
 --
 -- Completion engine at ./completion_engine.lua
 --
 -- LSP configurations at ./lspconfig.lua
---
--- LSP Saga UI at ./lspsaga.lua
 --
 -- Snippets controls at ./snippets.vim
--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,34 +1,46 @@
+-- Initialize LSPSaga
+require("lspsaga").init_lsp_saga();
 -- Mappings. See `:help vim.diagnostic.*` for documentation on any of the below functions
+local keymap = vim.keymap.set
+
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+keymap('n', '<space>e', vim.diagnostic.open_float, opts)
+keymap('n', '[d', vim.diagnostic.goto_prev, opts)
+keymap('n', ']d', vim.diagnostic.goto_next, opts)
+keymap('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
+  keymap('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  keymap('n', 'gd', vim.lsp.buf.definition, bufopts)
+  -- keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+  keymap('n', 'K', vim.lsp.buf.hover, bufopts)
+  -- keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+  keymap('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  -- Lsp finder find the symbol definition implement reference
+  -- if there is no implement it will hide
+  -- when you use action in finder like open vsplit then you can
+  -- use <C-t> to jump back
+  keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+  keymap('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  keymap('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  keymap('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  keymap('n', '<space>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rr', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  keymap('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  -- keymap('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  keymap({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+  keymap('n', 'gr', vim.lsp.buf.references, bufopts)
+  keymap('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  -- keymap('n', '<leader>rr', vim.lsp.buf.rename, bufopts)
+  keymap("n", "<leader>rr", "<cmd>Lspsaga rename<CR>", { silent = true })
 end
 
 -- Common UI settings related to LSP
@@ -67,19 +79,62 @@ autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
 
 -- LSP setup per language
+-- 
+-- Common flags
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
+
+-- TYPESCRIPT
 require('lspconfig')['tsserver'].setup{
-    on_attach = on_attach,
+    on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+        if client.server_capabilities.documentFormattingProvider then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("Format", { clear = true }),
+            buffer = bufnr,
+            callback = function() vim.lsp.buf.formatting_seq_sync() end
+          })
+        end
+        keymap('n', '<leader>rr', ':lua vim.lsp.buf.rename()<cr>')
+        keymap('n', '<leader>fmt', ":Neoformat prettier<cr>")
+    end,
     flags = lsp_flags,
+    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "typescript-language-server", "--stdio" }
 }
--- RUST is set up in discreet Rust file
+
+--SVELTE 
 require'lspconfig'.svelte.setup{
   on_attach = on_attach,
   flags = lsp_flags,
 }
+vim.g.vim_svelte_plugin_use_typescript = 1
+
+-- RUST
+local rt = require('rust-tools')
+require("rust-tools").setup({
+  server = {
+    on_attach = function(client, bufnr)
+      -- Hover actions
+      keymap("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      keymap("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      -- easy run code
+      keymap('n', '<leader>ru', ":!cargo run<cr>")
+      -- easy format
+      keymap('n', '<leader>fmt', ":!cargo fmt<cr>")
+      -- add semicolon easily
+      keymap('n', '<leader>;', "$a;<esc>o")
+
+      -- add keymaps for the rest of things
+      on_attach(client, bufnr)
+    end,
+  },
+})
+
+-- GOLANG
 local util = require'lspconfig/util'
 require'lspconfig'.gopls.setup {
   cmd = {'gopls', 'serve'},
@@ -87,6 +142,7 @@ require'lspconfig'.gopls.setup {
   root_dir = util.root_pattern('go.work', 'go.mod', '.git')
 }
 
+-- LUA
 require'lspconfig'.sumneko_lua.setup {
   cmd = {'/home/nikp/lua/bin/lua-language-server'},
   settings = {
@@ -104,5 +160,8 @@ require'lspconfig'.sumneko_lua.setup {
         enable = false
       }
     },
+  },
+  server = {
+    on_attach = on_attach
   }
 }
