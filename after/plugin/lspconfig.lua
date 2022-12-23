468e0c175edb0d1855582d2553f41ab589d889cc
nikfp <nikprotsman@gmail.com>
2022-12-23 12:16:18 -0700

    Cleanup after null-ls migration

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -60,17 +60,7 @@ nvim_lsp.clangd.setup({
 nvim_lsp.tsserver.setup({
 	on_attach = function(client, bufnr)
 		on_attach(client, bufnr)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = vim.api.nvim_create_augroup("Format", { clear = true }),
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.formatting_seq_sync()
-				end,
-			})
-		end
 		keymap("n", "<leader>rr", ":lua vim.lsp.buf.rename()<cr>")
-		-- keymap("n", "<leader>fmt", ":Neoformat prettier<cr>")
 	end,
 	flags = lsp_flags,
 	filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact" },
--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -3,7 +3,7 @@ local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
    null_ls.builtins.formatting.stylua,
-   null_ls.builtins.formatting.prettier.with({
+   null_ls.builtins.formatting.prettierd.with({
      extra_filetypes = { "svelte" }
    })
   }
