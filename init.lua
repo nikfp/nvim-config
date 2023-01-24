bafd5d10af9343a1c5368f0e9e3b4a3a5bac894c
nikfp <nikprotsman@gmail.com>
2023-01-23 21:56:21 -0700

    added mason installer

--- init.lua
+++ init.lua
@@ -1,12 +1,31 @@
 require("nikp.packer")
 require("nikp.set")
 require("mason").setup()
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"bash-language-server",
+		"codelldb",
+		"css-lsp",
+		"cssmodules-language-server",
+		"eslint-lsp",
+		"gopls",
+		"html-lsp",
+		"lua-language-server",
+    "node-debug2-adapter",
+		"prettierd",
+		"prisma-language-server",
+		"rust-analyzer",
+		"stylua",
+		"svelte-language-server",
+	},
+	auto_update = true,
+})
 require("chatgpt").setup({
 	yank_register = "r",
 })
 require("which-key").setup()
 require("mason-nvim-dap").setup({
-  ensure_installed = { "node2" }
+	ensure_installed = { "node2" },
 })
 require("nikp.dap").setup()
 
@@ -16,6 +35,6 @@ P = function(v)
 end
 
 Print = function(item)
-    vim.pretty_print(item)
-    return item
+	vim.pretty_print(item)
+	return item
 end
