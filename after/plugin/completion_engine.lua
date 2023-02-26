6094a5bdd6245fa97e19ea29b8c02670a51f0354
nikfp <nikprotsman@gmail.com>
2023-02-25 21:23:52 -0700

    After folder cleanups

--- after/plugin/completion_engine.lua
+++ after/plugin/completion_engine.lua
@@ -12,15 +12,10 @@ cmp.setup({
 			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
-	},
 	mapping = {
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
-		-- ['<Tab>'] = cmp.mapping.confirm({ select = true }),
 		["<esc>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.abort()
--- after/plugin/netrw.lua
+++
@@ -1,8 +0,0 @@
-require("netrw").setup({
-	icons = {
-		symlink = "", -- Symlink icon (directory and file)
-		directory = "", -- Directory icon
-		file = "", -- File icon
-	},
-	use_devicons = true,
-})
