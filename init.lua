e8c92b17c24013033bf5ee97f732b96146c3b0bb
nikfp <nikprotsman@gmail.com>
2023-02-27 16:43:33 -0700

    Added initial config files

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
+-- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not vim.loop.fs_stat(lazypath) then
+--   vim.fn.system({
+--     "git",
+--     "clone",
+--     "--filter=blob:none",
+--     "https://github.com/folke/lazy.nvim.git",
+--     "--branch=stable", -- latest stable release
+--     lazypath,
+--   })
+-- end
+-- vim.opt.rtp:prepend(lazypath)
 require("nikp.core.startup")
 
 P = function(v)
@@ -9,3 +21,5 @@ Print = function(item)
 	vim.pretty_print(item)
 	return item
 end
+
+-- require("lazy").setup("plugins");
