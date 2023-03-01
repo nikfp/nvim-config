f9128b6654fee68a7f80ffcfe5bac00a4e6e8017
nikfp <nikprotsman@gmail.com>
2023-03-01 14:05:44 -0700

    Turned on Lazy

--- init.lua
+++ init.lua
@@ -1,16 +1,15 @@
--- local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
--- if not vim.loop.fs_stat(lazypath) then
---   vim.fn.system({
---     "git",
---     "clone",
---     "--filter=blob:none",
---     "https://github.com/folke/lazy.nvim.git",
---     "--branch=stable", -- latest stable release
---     lazypath,
---   })
--- end
--- vim.opt.rtp:prepend(lazypath)
-require("nikp.core.startup")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
 P = function(v)
 	print(vim.inspect(v))
@@ -22,4 +21,5 @@ Print = function(item)
 	return item
 end
 
--- require("lazy").setup("plugins");
+require("lazy").setup("plugins");
+require("nikp.core.startup")
