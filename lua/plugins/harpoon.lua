c6d188e3e1affc7c9cac8be37093391ee7049438
nikfp <nikprotsman@gmail.com>
2023-03-23 07:53:57 -0600

    made harpoon wider

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,4 +4,11 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  config = function()
+    require("harpoon").setup({
+      menu = {
+        width = 80,
+      },
+    })
+  end,
 }
