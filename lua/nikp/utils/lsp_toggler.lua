9709894fc8e5c18493483ee749a3ad1724722c5b
nikfp <nikprotsman@gmail.com>
2023-06-06 19:58:59 +0000

    cleaned up the toggler a bit

--- lua/nikp/utils/lsp_toggler.lua
+++ lua/nikp/utils/lsp_toggler.lua
@@ -3,20 +3,18 @@ local M = {}
 M.get_lsp_num = function(lsp_name)
   local clients = vim.lsp.get_active_clients({ bufnr = 0 })
 
-  local out = {}
-  local out_length = 0
+  local out = nil
 
   for _, v in pairs(clients) do
     if (v.name == lsp_name) then
-      out[0] = v
-      out_length = out_length + 1
+      out = v
     end
   end
 
-  if (out_length == 0) then
+  if (out == nil) then
     return 0
   else
-    return out[0].id
+    return out.id
   end
 end
 
