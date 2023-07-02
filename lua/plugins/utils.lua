a377c14b265402e0aed588c240d0251686bcc67a
nikfp <nikfp@Niks-MBP.hsd1.co.comcast.net>
2023-07-02 13:12:24 -0600

    Removed chatgpt

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -94,16 +94,4 @@ return {
       require("no-neck-pain").setup()
     end,
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-  },
 }
