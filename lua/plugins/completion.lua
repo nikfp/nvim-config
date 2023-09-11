9b169cdf964155f4e8ea8d578f7991bf08b4b892
nikfp <nikfp@Niks-MBP.hsd1.co.comcast.net>
2023-09-11 14:24:56 -0600

    cleaned up completion file

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -18,12 +18,8 @@ return {
 
     cmp.setup({
       snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-          -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
           require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-          -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-          -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
         end,
       },
       mapping = {
@@ -110,32 +106,6 @@ return {
       completion = {
         autocomplete = false,
       },
-    })
-
-    -- Set configuration for specific filetype.
-    cmp.setup.filetype("gitcommit", {
-      sources = cmp.config.sources({
-        { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-      }, {
-        { name = "buffer" },
-      }),
-    })
-
-    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline({ "/", "?" }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-        { name = "cmdline" },
-      }),
     })
 
     vim.cmd([[
