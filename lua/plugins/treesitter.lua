f629a77e2e625460cd2f3a7f60fd3f01822d6481
nikfp <nik@nikfp.com>
2024-05-24 22:18:54 +0000

    ts text objects are fun

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,57 +1,102 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = "BufAdd",
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring"
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "bash",
-        "css",
-        "heex",
-        "html",
-        "javascript",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "regex",
-        "rust",
-        "svelte",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-      },
-      auto_install = true,
-      sync_install = false,
-      ignore_install = {},
-      modules = {},
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      rainbow = {
-        enable = true,
-        extended_mode = true,
-        max_file_lines = nil,
-      },
-      autotag = {
-        enable = true,
-        enable_close_on_slash = false
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn", -- set to `false` to disable one of the mappings
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufAdd",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring"
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "css",
+          "heex",
+          "html",
+          "javascript",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "regex",
+          "rust",
+          "svelte",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
         },
-      },
-    })
+        auto_install = true,
+        sync_install = false,
+        ignore_install = {},
+        modules = {},
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+        rainbow = {
+          enable = true,
+          extended_mode = true,
+          max_file_lines = nil,
+        },
+        autotag = {
+          enable = true,
+          enable_close_on_slash = false
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gnn", -- set to `false` to disable one of the mappings
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+          },
+        },
+      })
 
-    require("ts_context_commentstring").setup({})
-  end,
+      require("ts_context_commentstring").setup({})
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "BufAdd",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter"
+    },
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        textobjects = {
+          select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              -- You can also use captures from other query groups like `locals.scm`
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V', -- linewise
+              ['@class.outer'] = 'V', -- blockwise
+            },
+          },
+        },
+      }
+    end
+  }
 }
