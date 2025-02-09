60c0f19478d73c23d479f5bc9e77e9e51bc6d520
nikfp <nik@nikfp.com>
2025-02-09 16:33:58 -0700

    cleanups and niceties

---
+++ ftplugin/codecompanion.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "j", "gj", { buffer = 0 })
+vim.keymap.set("n", "k", "gk", { buffer = 0 })
+vim.keymap.set("n", "L", "g$", { buffer = 0 })
+vim.keymap.set("n", "H", "g^", { buffer = 0 })
--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -8,9 +8,28 @@ return {
     event = "UIEnter",
     config = function()
       require("codecompanion").setup({
+        display = {
+          chat = {
+            window = {
+              number = true,
+              numberwidth = 3,
+              relativenumber = true,
+              linebreak = true,
+              breakindent = true
+            }
+          }
+        },
         strategies = {
           chat = {
             adapter = "ollama",
+            keymaps = {
+              stop = {
+                modes = { n = "<C-c>", i = "<C-c>" }
+              },
+              close = {
+                modes = { n = "q" }
+              }
+            },
             slash_commands = {
               ["file"] = {
                 callback = "strategies.chat.slash_commands.file",
@@ -30,7 +49,7 @@ return {
           ollama = function()
             return require("codecompanion.adapters").extend("openai_compatible", {
               env = {
-                url = "http://localhost:1234", -- optional: default value is ollama url http://127.0.0.1:11434
+                url = "http://localhost:1234",     -- optional: default value is ollama url http://127.0.0.1:11434
                 api_key = "OpenAI_API_KEY",        -- optional: if your endpoint is authenticated
                 chat_url = "/v1/chat/completions", -- optional: default value, override if different
               },
