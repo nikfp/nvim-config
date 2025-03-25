34742d577113c1795dbf9b87d857b51f899ee267
Nik FP <nik@nikfp.com>
2025-03-25 17:00:59 -0600

    updates

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,7 +6,7 @@ return {
   event = "VeryLazy",
 
   -- use a release tag to download pre-built binaries
-  version = '0.11.0',
+  version = "1.*",
   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
